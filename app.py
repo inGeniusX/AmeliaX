c3fc0c4efb70b13668f7bea86d0c82a82b8ab680
Rayan Yu <rayan.yu430@gmail.com>
2020-04-28 11:33:35 -0400

    del

--- app.py
+++ app.py
@@ -73,6 +73,6 @@ def get_bot_response():
 
 
 if __name__ == "__main__":
-    app.run()
-    #from elsa import cli
-    #cli(app, base_url='https://ingeniusx.github.io/AmeliaX/')
+    #app.run()
+    from elsa import cli
+    cli(app, base_url='https://ingeniusx.github.io/AmeliaX/')
